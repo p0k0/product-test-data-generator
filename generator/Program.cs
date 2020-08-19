a5a7cfbd542aeff74a8d74f75434231eb42baddf
bmuhametshin <m11e@ya.ru>
2020-08-19 19:44:20 +0500

    command line parser added

--- generator/Program.cs
+++ generator/Program.cs
@@ -1,16 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using lib;
+using CommandLine;
+using CommandLine.Text;
 
 namespace generator
 {
+    class Options
+    {
+        [Option("stdout", Default = false, HelpText = "Write to stdout")]
+        public bool stdout { get; set; }
+
+        [Option('n', "name", HelpText = "Output file name")]
+        public string fileName { get; set; }
+
+        [Usage(ApplicationAlias = "generator.exe")]
+        public static IEnumerable<Example> Examples 
+        {
+            get 
+            {
+                yield return new Example("generate to file", UnParserSettings.WithGroupSwitchesOnly(), new Options{ fileName = "products.dat" });
+                yield return new Example("generate to stdout", UnParserSettings.WithGroupSwitchesOnly(), new Options{ stdout = true });
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "products.dat");
+            Parser.Default
+                .ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                    {
+                        if (o.stdout)
+                            WriteToStdout();
+                        else
+                            WriteToFile(o.fileName);
+                    }
+                );
+        }
+
+        static void WriteToStdout()
+        {
             var separateChar = ';';
-            var generator = new Generator(filePath, separateChar);
-            generator.FillRandomly();
+            var generator = new Generator(separateChar);
+            
+            using(Stream stream = Console.OpenStandardOutput())
+            {
+                generator.WriteRandomData(stream);
+            }
+        }
+
+        static void WriteToFile(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentException($"'{nameof(fileName)}' cannot be null or empty", nameof(fileName));
+
+            var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, fileName);
+            var separateChar = ';';
+            var generator = new Generator(separateChar);
+
+            using(var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                generator.WriteRandomData(fileStream);
+            }
+
         }
     }
 }
