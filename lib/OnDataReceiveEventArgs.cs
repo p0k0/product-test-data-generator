7df635807a0b04b35bb5a702fdd8722020c64d26
bmuhametshin <m11e@ya.ru>
2020-08-19 12:13:04 +0500

    removed unused constructor

--- lib/OnDataReceiveEventArgs.cs
+++ lib/OnDataReceiveEventArgs.cs
@@ -6,18 +6,6 @@ namespace lib
         public string Name;
         public double Price;
 
-        public OnDataReceiveEventArgs()
-        {
-            
-        }
-
-        public OnDataReceiveEventArgs(string order, string name, string price)
-        {
-            Order = order;
-            Name = name;
-            Price = System.Double.Parse(price);
-        }
-
         public void Init(string order, string name, string price)
         {
             Order = order;
