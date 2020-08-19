6258af59d051dfa2b7b27bb0bd635f055dc5bf87
bmuhametshin <m11e@ya.ru>
2020-08-19 12:58:03 +0500

    add productBucket

---
+++ lib/ProductBucket.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace lib
+{
+    internal class ProductBucket
+    {
+        public double MinPrice { get; }
+        public double MaxPrice { get; }
+
+        public ProductBucket(double minPrice, double maxPrice)
+        {
+            this.products = new List<Product>();
+            MinPrice = minPrice;
+            MaxPrice = maxPrice;
+        }
+
+        public void Add(Product newProduct)
+        {
+            if (MinPrice <= newProduct.Price &&
+                            newProduct.Price < MaxPrice)
+            {
+                this.products.Add(newProduct);
+            }
+            else 
+                throw new System.Exception("This product can`t be placed into this productBucket.");
+        }
+
+        public void WriteAtFile(string filePath, char separatorChar)
+        {
+            this.products.Sort();
+            using (var streamWriter = new System.IO.StreamWriter(filePath))
+            {
+                this.products.ForEach(product => product.Visit(streamWriter, separatorChar));
+            }
+        }
+
+        private List<Product> products;
+    }
+}
